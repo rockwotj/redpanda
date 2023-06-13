2d015a5d93db6f6a316f114d08df4de43b3a4436
Tyler Rockwood <rockwood@redpanda.com>
2023-06-13 12:49:39 -0500

    Smaller zips
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- tools/dogfood.py
+++ tools/dogfood.py
@@ -32,12 +32,14 @@ def release_rpk(args):
         subprocess.check_call(["task", "rpk:build", f"GOOS={goos}", f"GOARCH={goarch}"])
     build_dir = pathlib.Path(__file__).parent.parent / "vbuild/go"
     os.chdir(build_dir)
-    subprocess.check_call(["zip", "-r", "rpk-linux-amd64", "linux"])
-    subprocess.check_call(["zip", "-r", "rpk-darwin-arm64", "darwin"])
+    subprocess.check_call(["zip", "-r", "rpk-linux-amd64", "linux/amd64"])
+    subprocess.check_call(["zip", "-r", "rpk-darwin-arm64", "darwin/arm64"])
+    subprocess.check_call(["zip", "-r", "rpk-linux-arm64", "linux/arm64"])
+    subprocess.check_call(["zip", "-r", "rpk-darwin-amd64", "darwin/amd64"])
     subprocess.check_call(["gh", "release", "create",
                            "--repo", "rockwotj/redpanda",
                            f"wasmdev-{latest_release + 1}",
-                           "rpk-linux-amd64.zip", "rpk-darwin-arm64.zip"])
+                           "rpk-linux-amd64.zip", "rpk-darwin-arm64.zip", "rpk-darwin-amd64.zip", "rpk-linux-arm64.zip"])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
