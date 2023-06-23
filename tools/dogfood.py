156a03fac773a767d57bcd493f8a1024f8a5e7b7
Tyler Rockwood <rockwood@redpanda.com>
2023-06-23 13:42:05 -0500

    Simplify dogfood script
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- tools/dogfood.py
+++ tools/dogfood.py
@@ -9,17 +9,15 @@ import os
 
 BASE_DOCKER_IMAGE = "us-central1-docker.pkg.dev/rp-byoc-tyler/wasm-feature-branch/redpanda"
 
-def build_docker_image(args):
-    subprocess.check_call(["task", "rp:clean-pkg", "rp:build-docker-image", "BUILD_TYPE=release", "PKG_FORMATS=deb"])
-
-def tag_docker_image(args):
+def release_redpanda(args):
+    env = {**os.environ}
+    env["TARGETS"] = "redpanda rputil"
+    subprocess.check_call(["task", "rp:clean-pkg", "rp:build-docker-image", "BUILD_TYPE=release", "PKG_FORMATS=deb"], env=env)
     output = json.loads(subprocess.check_output(["docker", "inspect", "localhost/redpanda:dev"]))
     arch: str = output[0]["Architecture"]
     tag = f"{BASE_DOCKER_IMAGE}:{arch}"
     subprocess.check_call(["docker", "tag", "localhost/redpanda:dev", tag])
     subprocess.check_call(["docker", "push", tag])
-
-def upload_deb(args):
     build_dir = pathlib.Path(__file__).parent.parent / "vbuild/release/clang/dist/debian/"
     deb_files = list(build_dir.glob("*.deb"))
     assert len(deb_files) == 1, f"Expected only a single deb file, found: {deb_files}"
@@ -28,14 +26,11 @@ def upload_deb(args):
 def release_rpk(args):
     output = subprocess.check_output(["gh", "release", "list", "--repo", "rockwotj/redpanda", "--limit", "1"], text=True)
     latest_release = int(output.split("\t")[0].split("-")[1])
-    for (goos, goarch) in [("linux", "amd64"), ("darwin", "arm64"), ("linux", "arm64"), ("darwin", "amd64")]:
-        subprocess.check_call(["task", "rpk:build", f"GOOS={goos}", f"GOARCH={goarch}"])
     build_dir = pathlib.Path(__file__).parent.parent / "vbuild/go"
     os.chdir(build_dir)
-    subprocess.check_call(["zip", "-r", "rpk-linux-amd64", "linux/amd64"])
-    subprocess.check_call(["zip", "-r", "rpk-darwin-arm64", "darwin/arm64"])
-    subprocess.check_call(["zip", "-r", "rpk-linux-arm64", "linux/arm64"])
-    subprocess.check_call(["zip", "-r", "rpk-darwin-amd64", "darwin/amd64"])
+    for (goos, goarch) in [("linux", "amd64"), ("darwin", "arm64"), ("linux", "arm64"), ("darwin", "amd64")]:
+        subprocess.check_call(["task", "rpk:build", f"GOOS={goos}", f"GOARCH={goarch}"])
+        subprocess.check_call(["zip", "-r", f"rpk-{goos}-{goarch}", "{goos}/{goarch}"])
     subprocess.check_call(["gh", "release", "create",
                            "--repo", "rockwotj/redpanda",
                            f"wasmdev-{latest_release + 1}",
@@ -44,14 +39,10 @@ def release_rpk(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(required=True)
-    parser_foo = subparsers.add_parser('tag_docker_image')
-    parser_foo.set_defaults(func=tag_docker_image)
     parser_foo = subparsers.add_parser('release_rpk')
     parser_foo.set_defaults(func=release_rpk)
-    parser_foo = subparsers.add_parser('build_docker_image')
-    parser_foo.set_defaults(func=build_docker_image)
-    parser_foo = subparsers.add_parser('upload_deb')
-    parser_foo.set_defaults(func=upload_deb)
+    parser_foo = subparsers.add_parser('release_redpanda')
+    parser_foo.set_defaults(func=release_redpanda)
     args = parser.parse_args()
     if not hasattr(args, 'func'):
             print('Unrecognized command', file=sys.stderr)
