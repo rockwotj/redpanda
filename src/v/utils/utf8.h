76c4da6db9265a565f875b25ef8b58f547353d6d
Tyler Rockwood <rockwood@redpanda.com>
2023-07-17 16:25:42 -0500

    Add a utility to check if a string is valid UTF8 without exceptions
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- src/v/utils/utf8.h
+++ src/v/utils/utf8.h
@@ -107,15 +107,27 @@ inline void validate_no_control(std::string_view s) {
 
 template<typename Thrower>
 requires ExceptionThrower<Thrower>
-inline void validate_utf8(std::string_view s, Thrower&& thrower) {
+inline void validate_utf8(std::string_view s, Thrower* thrower) {
     try {
         boost::locale::conv::utf_to_utf<char>(
           s.begin(), s.end(), boost::locale::conv::stop);
     } catch (const boost::locale::conv::conversion_error& ex) {
-        thrower.conversion_error();
+        thrower->conversion_error();
     }
 }
 
 inline void validate_utf8(std::string_view s) {
-    validate_utf8(s, default_utf8_thrower{});
+    default_utf8_thrower thrower{};
+    validate_utf8(s, &thrower);
+}
+
+inline bool is_valid_utf8(std::string_view s) {
+    struct default_utf8_checker {
+        bool valid = true;
+        [[gnu::cold]] void conversion_error() { valid = false; }
+    };
+
+    default_utf8_checker checker{};
+    validate_utf8(s, &checker);
+    return checker.valid;
 }
