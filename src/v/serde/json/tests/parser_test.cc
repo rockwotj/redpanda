c7f3225862e4861ff00517b4c724cd01e3bac7c9
Nicolae Vartolomei <nv@redpanda.com>
2025-05-08 17:38:27 +0100

    serde/json: json-test-suite test

--- src/v/serde/json/tests/parser_test.cc
+++ src/v/serde/json/tests/parser_test.cc
@@ -9,4 +9,25 @@
  * by the Apache License, Version 2.0
  */
 
+#include "serde/json/parser.h"
+#include "serde/json/tests/data.h"
+#include "test_utils/test.h"
+
 #include <gtest/gtest.h>
+
+using namespace experimental::serde::json;
+
+// Simple test to ensure the parse doesn't fail on valid sample data. The
+// contents and correctness is not verified in this test.
+TEST_CORO(json_test_suite, parse) {
+    auto parser = experimental::serde::json::parser(
+      co_await json_test_suite_sample());
+
+    while (co_await parser.next()) {
+        // Do nothing, just drain the parser.
+        // The contents and correctness is not verified in this test.
+    }
+
+    EXPECT_EQ(parser.token(), token::eof) << "Expected to reach EOF but got: "
+                                          << std::to_underlying(parser.token());
+}
