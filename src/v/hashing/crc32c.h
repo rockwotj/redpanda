1eabf52e4e85e5d1c85e89588851ea1a9776a3c1
Tyler Rockwood <rockwood@redpanda.com>
2025-06-18 02:24:03 +0000

    hashing: add crc masking routines
    As recommended by leveldb authors, mask nested crcs.

--- src/v/hashing/crc32c.h
+++ src/v/hashing/crc32c.h
@@ -41,6 +41,24 @@ private:
     uint32_t _crc = 0;
 };
 
+static const uint32_t mask_delta = 0xa282ead8ul;
+
+// Return a masked representation of crc.
+//
+// Motivation: it is problematic to compute the CRC of a string that
+// contains embedded CRCs. Therefore we recommend that CRCs stored
+// somewhere (e.g., in files) should be masked before being stored.
+inline uint32_t mask(uint32_t crc) {
+    // Rotate right by 15 bits and add a constant.
+    return ((crc >> 15) | (crc << 17)) + mask_delta;
+}
+
+// Return the crc whose masked representation is masked_crc.
+inline uint32_t unmask(uint32_t masked_crc) {
+    uint32_t rot = masked_crc - mask_delta;
+    return ((rot >> 17) | (rot << 15));
+}
+
 } // namespace crc
 
 inline void crc_extend_iobuf(crc::crc32c& crc, const iobuf& buf) {
