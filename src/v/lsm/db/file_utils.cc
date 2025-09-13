74ac36c87b7eea68f6fd356d6d499b05d878f6f8
Tyler Rockwood <rockwood@redpanda.com>
2025-09-13 03:33:04 +0000

    lsm: add boundary compaction getter

--- src/v/lsm/db/file_utils.cc
+++ src/v/lsm/db/file_utils.cc
@@ -96,4 +96,58 @@ bool some_file_overlaps_range(
     return !before_file(*files[index], largest_key);
 }
 
+namespace {
+
+std::optional<internal::key_view> find_largest_key(
+  const chunked_vector<ss::lw_shared_ptr<file_meta_data>>& files) {
+    auto it = std::ranges::max_element(
+      files, std::less{}, [](const ss::lw_shared_ptr<file_meta_data>& file) {
+          return internal::key_view{file->largest};
+      });
+    if (it == files.end()) {
+        return std::nullopt;
+    }
+    return internal::key_view{(*it)->largest};
+}
+
+ss::lw_shared_ptr<file_meta_data> find_smallest_boundary_file(
+  const chunked_vector<ss::lw_shared_ptr<file_meta_data>>& level_files,
+  internal::key_view largest_key) {
+    ss::lw_shared_ptr<file_meta_data> smallest_boundary_file = nullptr;
+    for (const auto& file : level_files) {
+        if (
+          file->smallest > largest_key
+          && file->smallest.user_key() == largest_key.user_key()) {
+            if (
+              !smallest_boundary_file
+              || file->smallest < smallest_boundary_file->smallest) {
+                smallest_boundary_file = file;
+            }
+        }
+    }
+    return smallest_boundary_file;
+}
+
+} // namespace
+
+void add_boundary_inputs(
+  const chunked_vector<ss::lw_shared_ptr<file_meta_data>>& files,
+  chunked_vector<ss::lw_shared_ptr<file_meta_data>>* compaction_files) {
+    auto largest_key = find_largest_key(*compaction_files);
+    if (!largest_key) {
+        return;
+    }
+    bool continue_searching = true;
+    while (continue_searching) {
+        auto smallest_boundary_file = find_smallest_boundary_file(
+          files, *largest_key);
+        if (smallest_boundary_file != nullptr) {
+            largest_key = smallest_boundary_file->largest;
+            compaction_files->push_back(std::move(smallest_boundary_file));
+        } else {
+            continue_searching = false;
+        }
+    }
+}
+
 } // namespace lsm::db
