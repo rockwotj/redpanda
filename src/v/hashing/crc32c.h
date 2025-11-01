cad61038865ad411c0cdf32e4ca4810aab2e986c
Tyler Rockwood <rockwood@redpanda.com>
2025-11-01 17:50:57 +0000

    hashing/crc32c: add mask/unmask
    When storing a crc of crc, it's good to mask it as stated in the
    comments.
    
    Ported from: https://github.com/google/leveldb/blob/ac691084fdc5546421a55b25e7653d450e5a25fb/util/crc32c.h#L22-L37

--- src/v/hashing/crc32c.h
+++ src/v/hashing/crc32c.h
@@ -45,6 +45,24 @@ private:
     value_type _crc = 0;
 };
 
+static const crc32c::value_type mask_delta = 0xa282ead8ul;
+
+// Return a masked representation of crc.
+//
+// Motivation: it is problematic to compute the CRC of a string that
+// contains embedded CRCs. Therefore we recommend that CRCs stored
+// somewhere (e.g., in files) should be masked before being stored.
+inline crc32c::value_type mask(crc32c::value_type crc) {
+    // Rotate right by 15 bits and add a constant.
+    return ((crc >> 15) | (crc << 17)) + mask_delta;
+}
+
+// Return the crc whose masked representation is masked_crc.
+inline crc32c::value_type unmask(crc32c::value_type masked_crc) {
+    uint32_t rot = masked_crc - mask_delta;
+    return ((rot >> 17) | (rot << 15));
+}
+
 } // namespace crc
 
 inline void crc_extend_iobuf(crc::crc32c& crc, const iobuf& buf) {
